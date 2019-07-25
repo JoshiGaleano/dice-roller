1e684f5e0e91c1ea2385085cc070766d386da5fc
Josimar Galeano <josimar.galeano@globant.com>
2019-07-25 11:16:36 -0500

    default project to android

---
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.diceroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //https://dl.google.com/dl/android/maven2/index.html
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
