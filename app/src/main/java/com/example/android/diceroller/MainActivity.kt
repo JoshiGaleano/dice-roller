ca13823e7d15b8cfe6698dda1615ec24bacabce4
Josimar Galeano <josimar.galeano@globant.com>
2019-07-29 10:42:23 -0500

    show a random number in a TextView

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.android.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     //https://dl.google.com/dl/android/maven2/index.html
@@ -9,5 +13,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
